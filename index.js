d4b5231adfc8d6d3fce6c4a6d51f5642bea674d3
ved3088 <vedantphulkar123@gmail.com>
2024-12-10 17:36:01 +0530

    expenses calculated

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const accountData = require('./data.json');
 const accounts = accountData.data
 
-
+// calculate revenue
 const revenue = accounts
   .filter(account => account.account_category === "revenue")
   .reduce((sum, account) => sum + account.total_value, 0);
 
-
+// calculate expenses
+const expenses = accounts
+  .filter(account => account.account_category === "expense")
+  .reduce((sum, account) => sum + account.total_value, 0);
 
 
 const formatter = new Intl.NumberFormat('en-US');
 
-  
+
 console.log(`Revenue: $${formatter.format(revenue.toFixed(0))}`);
+console.log(`Expenses: $${formatter.format(expenses.toFixed(0))}`);
--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,9 @@ const testData = require('./test.json');
 const formatter = new Intl.NumberFormat('en-US');
 
 describe("Financial calculations: ", () => {
-  describe("Revenue generated: ", () => {
+
+
+  describe("Revenue calculated: ", () => {
     it("should calculate total revenue ", () => {
       const rev = testData
         .filter(account => account.account_category === "revenue")
@@ -11,6 +13,22 @@ describe("Financial calculations: ", () => {
       const revenue = formatter.format(rev)
       expect(revenue).toBe('50,000')
     });
+  });
+
+  describe("Expenses calculated: ", () => {
+    it("should calculate total expenses ", () => {
+      const exp = testData
+        .filter(account => account.account_category === "expense")
+        .reduce((sum, account) => sum + account.total_value, 0);
+      const expenses = formatter.format(exp)
+      expect(expenses).toBe('20,000')
+    });
+  });
+
+  describe("Gross Profit Margin: ", () => {
+    it("should calculate gross profit margin ", () => {
+      it.todo("calculate gross profit margin")
+    })
   })
 })
 
