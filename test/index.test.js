efb996e131fea6807ae40a5df9dd372173505e30
ved3088 <vedantphulkar123@gmail.com>
2024-12-10 17:26:23 +0530

    initial commit

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+
+describe("Financial calculations: ", () => {
+  describe("Revenue generated: ",() =>{
+    it("should calculate total revenue ", () => {
+      it.todo('calculate revenue generated');
+    });
+  })
+})
+
